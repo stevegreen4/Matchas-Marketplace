aa0a52cc3da369910223ad7760509256670eb60c
stevegreen4 <greensteve580@gmail.com>
2023-12-09 14:33:17 -0500

    Updated URL for database from Railway

--- src/services/ItemService.js
+++ src/services/ItemService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const http = axios.create({
-    baseURL: "http://localhost:9000"
+    baseURL: "matchasmarketplacedb-production.up.railway.app"
 });
 
 export default {
